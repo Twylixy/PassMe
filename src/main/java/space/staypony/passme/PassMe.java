58e66e2abb154aac0ef85e1a372c7a85f5719ce8
Twylix <zemskov271@gmail.com>
2022-06-28 17:39:24 +0500

    initial commit

---
+++ src/main/java/space/staypony/passme/PassMe.java
@@ -0,0 +1,17 @@
+package space.staypony.passme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PassMe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
